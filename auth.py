ab4c00abdd938808676953d3babfabf3e6ca6d01
Tran Vo <vobaohoangtran@gmail.com>
2026-06-23 19:31:13 +0000

    feat: added login function

--- auth.py
+++ auth.py
@@ -1,3 +1,8 @@
+import sqlite3
+from database import get_connection
+
+
+# user create account
 def register():
     username = input("Enter your username (): ")
     pin = input("Enter your PIN (4 digits): ")
@@ -15,15 +20,66 @@ def register():
         return
 
     pin = int(pin)
-    conn = sqlite3.connect("museum.db")
+    conn = get_connection()
     cursor = conn.cursor()
 
     try: 
         cursor.execute("""
         INSERT INTO users (username, pin)
         VALUES (?, ?)
-        """
+        """, (username, pin)
         )
         conn.commit()
+        print("Account created")
     except sqlite3.Error as e:
         print("User creation error!")
+    finally:
+        conn.close()
+
+
+# user login
+def login():
+    username = input("Enter your username (): ")
+    pin = input("Enter your PIN (4 digits): ")
+
+    if len(username) <= 4:
+        print("Username is too short (5 characters minimum)")
+        return
+
+    if not pin.isdigit():
+        print("PIN must contain only numbers")
+        return
+
+    if len(pin) != 4:
+        print("PIN must be exactly 4 digits")
+        return
+
+    pin = int(pin)
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    try: 
+        cursor.execute("""
+        SELECT id, username 
+        FROM users
+        WHERE username = ? and pin = ?
+        """, (username, pin)
+        )
+        
+        user = cursor.fetchone()
+
+        if user:
+            print(f"Welcome {username}")
+            return user
+
+        else:
+            print("Invalid username or PIN")
+            return None
+
+    except sqlite3.Error as e:
+        print("User creation error!")
+    finally:
+        conn.close()
+
+
+
---
+++ main.py
@@ -0,0 +1,9 @@
+from database import create_tables
+from auth import register, login
+
+create_tables()
+
+register()
+login()
+# menu/ flow for the app
+ 
