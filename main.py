afa0a1595fdafa94fc1c973bfbc7312757a7aaba
Tran Vo <vobaohoangtran@gmail.com>
2026-06-23 17:47:00 +0000

    feat: create user table

--- main.py
+++ main.py
@@ -2,3 +2,20 @@ import sqlite3
 
 conn = sqlite3.connect("museum.db")
 cursor = conn.cursor()
+
+# create user table 
+try:
+    cursor.execute(""" 
+    CREATE TABLE users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT
+        username TEXT UNIQUE
+        pin INTEGER 
+    )
+    """)
+    print("Database created!")
+except sqlite3.Error as e:
+    print("User table creation error!")
+
+
+conn.commit()
+conn.close()
