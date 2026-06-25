c5847f2a65b69ed74443461ee2fdd297ee11d2e8
Ahsan Rahul <rahul.ahsan10@gmail.com>
2026-06-25 17:14:33 +0000

    added color palette returning functionality

--- data_get_and_processing/palette_and_material_gen.py
+++ data_get_and_processing/palette_and_material_gen.py
@@ -1,6 +1,12 @@
 import os
-import requests
+from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv
+from museum_data_req import *
+from PIL import Image
+from io import BytesIO
+import requests as r
+from curl_cffi import requests
+from curl_cffi.requests.exceptions import HTTPError, RequestException
 
 load_dotenv()
 
@@ -13,19 +19,75 @@ if not imagga_key:
 
 color_gen_url = "https://api.imagga.com/v2/colors"
 
-'''
-workflow breakdown:
-1. user inputs art they want analyzed via CLI
-2. SQL query pulls all the image_urls from the SELECT query generated, puts it into an array
-3. parse through the array using the pillow module
-4. the images will be turned into a local strip of images
-5. pipe this output into imagga with a POST request
-'''
 
-'''
-response template: response = requests.get(
-    'https://api.imagga.com/v2/colors?image_url=https://imagga.com/static/images/colors/sample.jpg',
-    auth=('<api-key>', '<api-secret>'),
-)
-'''
+#this function is from the PIL Image documentation
+def merge(im1: Image.Image, im2: Image.Image) -> Image.Image:
+    w = im1.size[0] + im2.size[0]
+    h = max(im1.size[1], im2.size[1])
+    im = Image.new("RGB", (w, h))
 
+    im.paste(im1)
+    im.paste(im2, (im1.size[0], 0))
+
+    return im
+
+def create_image_strip(img_urls):
+    in_memory_images = [] 
+    for url in img_urls:
+        if url == "":
+            continue
+        try:
+            response = requests.get(url, impersonate="chrome120")
+            response.raise_for_status()
+            img = Image.open(BytesIO(response.content)).convert("RGB")
+            height = 400
+            aspect_ratio = img.width / img.height
+            width = int(height * aspect_ratio)
+            resized = img.resize((width, height))
+            in_memory_images.append(resized)
+        except Exception as err:
+            print(f'{err}, couldnt do {url}')
+
+    result = in_memory_images[0]
+    for imgs in in_memory_images:
+        result = merge(result, imgs)
+
+    result.convert("RGB").save("imagestrip.jpg", "JPEG")
+    return "imagestrip.jpg"
+
+
+def get_color_palette(imgfilename):
+    #and this is from the imagga docs:
+    with open(imgfilename, "rb") as f:
+        resp = r.post(
+            color_gen_url,
+            files={"image": f},
+            auth=HTTPBasicAuth(imagga_key, imagga_secret)
+        )
+    data = resp.json()
+
+    colors =  data['result']['colors']['image_colors']
+
+    top_five = sorted(colors, key=lambda x : x['percent'], reverse=True)[:5]
+
+    return top_five
+
+def display_palette(top_five):
+    top_5_dict = {}
+    print("\n Your Selected Work's Palette")
+    for c in top_five:
+        name = c['closest_palette_color']
+        hex_code = c['html_code']
+        top_5_dict[name] = [hex_code]
+    
+    print(f'- {name} | {hex_code}')
+
+# if __name__ == '__main__':
+    # c = get_country()
+    # urls = create_urls('France')
+    # working_df = create_works_df(urls)
+    # img_urls = working_df['image_url'].tolist()
+    # img_strip = create_image_strip(img_urls)
+    # top_five = get_color_palette(img_strip)
+    # print(top_five)
+    # display_palette(top_five)
