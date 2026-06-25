401126c6e3764368779194671d76ff9a3470a28c
Ahsan Rahul <rahul.ahsan10@gmail.com>
2026-06-25 20:46:32 +0000

    added yaml file and actions workflow hopefully

--- tests/test_museum_data.py
+++ tests/test_museum_data.py
@@ -16,13 +16,13 @@ class TestMuseumData(unittest.TestCase):
         test_urls = create_urls(country_name)
         test_chicago_dict = get_chicago_works(test_urls['chi'])
         for k, v in test_chicago_dict.items():
-            self.assertIn('https://www.artic.edu/artworks/', v[1])
+            self.assertIn('https://www.artic.edu/artworks/', v["website_link"])
 
 
-    @patch('data_get_and_processing.museum_data_req.create_works_df')
+    @patch('data_get_and_processing.museum_data_req.create_works_dict')
     def test_create_works_dict(self, mock_fetch):
         mock_dict = {
-            "work_{i}": [f"Artist {i}", "http://link.com", "http://image.com"] 
+            f"work_{i}": {'artist': f"Artist {i}", "website_link": "http://link.com", "image_link": "http://image.com"}
             for i in range(15)
         }
         mock_fetch.return_value = mock_dict
